1001a72a24f86688cb9d81a37619399f1882f6e0
git-kadar <98693137+git-kadar@users.noreply.github.com>
2022-01-30 19:00:22 +0530

    Update VariableDemo.java

--- mit-nc/src/VariableDemo.java
+++ mit-nc/src/VariableDemo.java
@@ -1,6 +1,6 @@
 import java.io.*;
 public class VariableDemo {
-
+// Change wii see eclipse wait....
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Declaring a variable with value
